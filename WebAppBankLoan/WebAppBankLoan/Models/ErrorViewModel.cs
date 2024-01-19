4279f4497f02ab2ed913cc2e2f56fca97e810662
Sricharan2211 <tudugani.charan@mphasis.com>
2024-01-19 12:41:49 +0530

    phase 4 practice project 3

---
+++ WebAppBankLoan/WebAppBankLoan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppBankLoan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
